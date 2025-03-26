ddadc3b79ddc8679bc30b122a9b4ebab3d868c91
Andreas Prouza <andreas@prouza.at>
2025-03-26 19:13:06 +0100

    Start extended source processing

--- module/app_config_tools.py
+++ module/app_config_tools.py
@@ -1,6 +1,10 @@
 import logging
 import pathlib
 import fnmatch
+import re
+
+from etc import constants
+from module import toml_tools, files, properties
 
 
 def get_steps(app_config: dict, source: str) -> list:
@@ -18,3 +22,50 @@ def get_steps(app_config: dict, source: str) -> list:
     logging.debug(f"{file_extensions=}")
 
     return app_config['global']['steps'].get(file_extensions, [])
+
+
+
+
+def get_extended_steps(app_config: dict, source: str) -> list:
+    """If extended steps exist, search for a match
+    Args:
+        app_config (properties): Application config file
+        source (str): source file name
+
+    Returns:
+        list: List of steps
+    """
+
+    conditions = {
+        'SOURCE_FILE_NAME': match_condition_SOURCE_FILE_NAME
+    }
+
+    sources_config=properties.get_config(constants.EXTENDED_SOURCE_PROCESS_CONFIG_TOML)
+    if len(sources_config) == 0:
+        return []
+
+    for proc_name, proc_dict in sources_config.items():
+
+        if 'conditions' not in proc_dict:
+            continue
+
+        found = True
+        for cond_name, cond_dict in proc_dict.items():
+        
+            # Every condition needs to match
+            found = conditions[cond_name](source, cond_dict)
+            if not found:
+                break
+
+        if found:
+            return step_values['steps']
+
+
+
+
+def match_condition_SOURCE_FILE_NAME(source:str, conditions) -> bool:
+
+    if conditions['settings']['use-regex']:
+        return re.search(conditions['filter'], source)
+
+    return fnmatch.fnmatch(source, conditions['filter'])
