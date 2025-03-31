7d421fd56eb60e6fabc40f7a2415626cd9a72818
Andreas Prouza <andreas@prouza.at>
2025-03-31 09:15:19 +0200

    Add new global variable 'SOURCE_BASE_FILE_NAME'

--- module/properties.py
+++ module/properties.py
@@ -94,6 +94,7 @@ def get_source_properties(config, source):
 
   global_settings.update(type_settings)
   global_settings['SOURCE_FILE_NAME'] = os.path.join(config['general']['source-dir'], source).replace('\\', '/')
+  global_settings['SOURCE_BASE_FILE_NAME'] = os.path.basename(global_settings['SOURCE_FILE_NAME'])
   global_settings['TARGET_LIB'] = get_target_lib(source, global_settings.get('TARGET_LIB'), global_settings.get('TARGET_LIB_MAPPING'))
   global_settings['OBJ_NAME'] = pathlib.Path(pathlib.Path(source).stem).stem
 
