bdd7ea48d755132255e2a4686a6aaeeb8313e205
Andreas Prouza <andreas@prouza.at>
2025-11-17 14:22:38 +0100

    Specific python version

---
+++ setup3.12.sh
@@ -0,0 +1,24 @@
+
+set -o errexit
+
+if [ -d "./venv" ]; then
+  echo "Virtual environment does already exist"
+else
+  echo "Create virtual environment"
+  python3.12 -m venv --system-site-packages ./venv
+fi
+
+echo "Activate virtual environment"
+if [ $OSTYPE == 'msys' ] || [ $OSTYPE == 'win32' ] || [ $OSTYPE == 'mingw' ] || [ $OSTYPE == 'mingw32' ]; then
+  source venv\\Scripts\\activate
+  PYTHON_PATH=venv\\Scripts\\python.exe
+else
+  source venv/bin/activate
+  PYTHON_PATH=venv/bin/python
+fi
+
+echo "Update pip"
+"$PYTHON_PATH" -m pip install --upgrade pip
+
+echo "Install all requirements"
+pip install -r requirements.txt
